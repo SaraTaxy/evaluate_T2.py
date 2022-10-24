512e34d23291afa79ae048b66ee4f6332cf3a284
SaraTaxy <sara5tassinari@gmail.com>
2022-10-24 17:31:58 +0200

    find the parallelepiped containing all image information finale

--- script_volum_images_T2.py
+++ script_volum_images_T2.py
@@ -283,78 +283,225 @@ import pandas as pd
 
 data_path = "data/raw/T2_images"
 
-d= {}
+d=[]
 p=0
-volum_images=[]
-n_images=[]
+
+#dict_mask = {}
 for images in os.listdir(data_path):
     dict_mask = {}
-    p=+1
-    path_image = os.path.join(data_path, images)
-    if path_image.find('DS_Store') != -1:
-        continue
-
-    T2_image = nib.load(path_image).get_fdata()
-    T2_image = np.rot90(T2_image)
-    nd = np.ndim(T2_image)
-    max_pixel = np.amax(T2_image)
-    min_pixel = np.amin(T2_image)
-
-    mask = ((T2_image[:,:,:])>0)
-
-    i_list = []
-    j_list = []
-    z_list = []
-
-    for z in range(0, mask.shape[2]):
-        for i in range(0, mask.shape[0]):
-            for j in range(0, mask.shape[1]):
-                if mask[i][j][z]==True:
-                    j_list.append(j)
-                    i_list.append(i)
-                    z_list.append(z)
 
 
+    if p<=10:
+        p = p + 1
+        print(p)
+        path_image = os.path.join(data_path, images)
+        if path_image.find('DS_Store') != -1:
+            continue
 
-    print("nome immagine: ", path_image)
-    print("x min: ", np.amin(i_list), "x max: ", np.amax(i_list))
-    print("y min: ", np.amin(j_list), "y max: ", np.amax(j_list))
-    print("z min: ", np.amin(z_list), "z max: ", np.amax(z_list))
+        T2_image = nib.load(path_image).get_fdata()
+        T2_image = np.rot90(T2_image)
+        nd = np.ndim(T2_image)
+        max_pixel = np.amax(T2_image)
+        min_pixel = np.amin(T2_image)
 
-    vol = (np.amax(i_list)-np.amin(i_list))*(np.amax(j_list)-np.amin(j_list))*(np.amax(z_list)-np.amin(z_list))
+        mask = ((T2_image[:,:,:])>0)
 
-    print("vol in pixel: ", vol)
-    dict_mask = {"num image": p,
-                 "image": images,
-                 "x": [np.amin(i_list), np.amax(i_list)],
-                 "y": [np.amin(j_list), np.amax(j_list)],
-                 "z": [np.amin(z_list), np.amax(z_list)],
-                 "Pixel_Vol": vol}
-    d[i]=dict_mask
+        i_list = []
+        j_list = []
+        z_list = []
 
-    volum_images.append(dict_mask["Pixel_Vol"])
-    n_images.append(dict_mask["image"])
+        for z in range(0, mask.shape[2]):
+            for i in range(0, mask.shape[0]):
+                for j in range(0, mask.shape[1]):
+                    if mask[i][j][z]==True:
+                        j_list.append(j)
+                        i_list.append(i)
+                        z_list.append(z)
 
-#find max value of images volum
 
-max_vol=0
-index_max_vol=0
-for q in range(0, len(volum_images)):
-    if volum_images[i]>max_vol:
-        max_vol=volum_images[i]
-        index_max_vol=q
+        '''
+        print("nome immagine: ", path_image)
+        print("x min: ", np.amin(i_list), "x max: ", np.amax(i_list))
+        print("y min: ", np.amin(j_list), "y max: ", np.amax(j_list))
+        print("z min: ", np.amin(z_list), "z max: ", np.amax(z_list))
+        '''
+        vol = (np.amax(i_list)-np.amin(i_list))*(np.amax(j_list)-np.amin(j_list))*(np.amax(z_list)-np.amin(z_list))
+
+        #print("vol in pixel: ", vol)
+        dict_mask = {"num image": p,
+                     "image": images,
+                     "x": [np.amin(i_list), np.amax(i_list)],
+                     "y": [np.amin(j_list), np.amax(j_list)],
+                     "z": [np.amin(z_list), np.amax(z_list)],
+                     "Pixel_Vol": vol,
+                     "delta_x": (np.amax(i_list) - np.amin(i_list)),
+                     "delta_y": (np.amax(j_list) - np.amin(j_list)),
+                     "delta_z": (np.amax(z_list) - np.amin(z_list))}
+        d.append(dict_mask)
     else:
-        max_vol= max_vol
+        p = p + 1
 
-print("vol max: ", max_vol, "image vol max: ", n_images[index_max_vol])
+print(d)
 
 
+max_x = 0
+index_x = 0
+max_y = 0
+index_y = 0
+max_z = 0
+index_z = 0
+for i in range(0, len(d)):
+    if d[i]['delta_x'] > max_x:
+        max_x = d[i]['x'][1] - d[i]['x'][0]
+        im_x=d[i]['image']
+        index_x = i
+    else:
+        max_x = max_x
+
+    if d[i]['delta_y'] > max_y:
+        max_y = d[i]['y'][1] - d[i]['y'][0]
+        im_y=d[i]['image']
+        index_y = i
+    else:
+        max_y = max_y
+
+    if d[i]['delta_z'] > max_z:
+        max_z = d[i]['z'][1] - d[i]['z'][0]
+        im_z=d[i]['image']
+        index_z = i
+    else:
+        max_z = max_z
+
+
+
+#crop image in the center
+x_star = int(max_x/2)
+y_star = int(max_y/2)
+z_star = int(max_z/2)
+
+
+for i in range(0, len(d)):
+
+    x_min_im = int(d[i]['x'][0]) + int(d[i]['delta_x']/2) - x_star
+    x_max_im = int(d[i]['x'][0]) + int(d[i]['delta_x']/2) + x_star
+    #print("delta_x im ", i)
+    #print([x_min_im, x_max_im])
+
+    y_min_im = int(d[i]['y'][0]) + int(d[i]['delta_y'] / 2) - y_star
+    y_max_im = int(d[i]['y'][0]) + int(d[i]['delta_y'] / 2) + y_star
+
+    z_min_im = int(d[i]['z'][0]) + int(d[i]['delta_z'] / 2) - z_star
+    z_max_im = int(d[i]['z'][0]) +int(d[i]['delta_z'] / 2) + z_star
+
+
+
+# create file
+with open("mask_dimension_T2_images.txt", 'w') as f:
+    for elem in d:
+        f.write(str(elem))
+        f.write('\n')
+    f.write('\n')
+
+    # find max value of images volum
+    f.write("----------------------------------------DIM MASK MAX VOLUM IMAGE-----------------------------------------------------")
+    f.write('\n')
+    f.write("volum max: ")
+    f.write(str(max))
+    f.write('\n')
+    f.write("index image x max: ")
+    f.write(str(im_x))
+    f.write('\n')
+    f.write("delta_x: ")
+    f.write(str(max_x))
+    f.write('\n')
+    f.write("index image y max: ")
+    f.write(str(im_y))
+    f.write('\n')
+    f.write("delta_y: ")
+    f.write(str(max_y))
+    f.write('\n')
+    f.write("index image z max: ")
+    f.write(str(im_z))
+    f.write('\n')
+    f.write("delta_z: ")
+    f.write(str(max_z))
+    f.write('\n')
+
+    # crop image in the center
+    x_star = int(max_x/2)
+    y_star = int(max_y/2)
+    z_star = int(max_z/2)
+
+    f.write("----------------------------------------END MASK FOR EACH IMAGES-----------------------------------------------------")
+    f.write('\n')
+
+    for i in range(0, len(d)):
+        x_min_im = int(d[i]['x'][0]) + int(d[i]['delta_x'] / 2) - x_star
+        x_max_im = int(d[i]['x'][0]) + int(d[i]['delta_x'] / 2) + x_star
+        # print("delta_x im ", i)
+        # print([x_min_im, x_max_im])
+
+        y_min_im = int(d[i]['y'][0]) +int(d[i]['delta_y'] / 2) - y_star
+        y_max_im = int(d[i]['y'][0]) + int(d[i]['delta_y'] / 2) + y_star
+
+        z_min_im = int(d[i]['z'][0]) + int(d[i]['delta_z'] / 2) - z_star
+        z_max_im = int(d[i]['z'][0]) + int(d[i]['delta_z'] / 2) + z_star
+
+
+        f.write("x_min: ")
+        f.write(str(x_min_im))
+        f.write(" x_max: ")
+        f.write(str(x_max_im))
+        f.write('\n')
+        f.write("y_min: ")
+        f.write(str(y_min_im))
+        f.write(" y_max: ")
+        f.write(str(y_max_im))
+        f.write('\n')
+        f.write("z_min: ")
+        f.write(str(z_min_im))
+        f.write(" z_max: ")
+        f.write(str(z_max_im))
+        f.write('\n')
+        f.write("------------------------------")
+        f.write('\n')
 
 
 with open("mask_dimension_T2_images.txt", 'w') as f:
-    for key, value in d.items():
-        f.write('%s:%s\n' % (key, value))
-    f.write("volume max: ", max_vol, "im vol max", n_images[index_max_vol])
+    for elem in d:
+        f.write(str(elem))
+        f.write('\n')
+    f.write('\n')
+    f.write("----------------------------------------DIM MASK MAX VOLUM IMAGE-----------------------------------------------------")
+    # find max value of images volum
+    max=0
+    index=0
+
+
+    for i in range(0, len(d)):
+        if d[i]['Pixel_Vol']> max:
+            max=d[i]['Pixel_Vol']
+            index=i
+        else:
+            max=max
+
+    f.write('\n')
+    f.write("volum max: ")
+    f.write(str(max))
+    f.write('\n')
+    f.write("index image vol max: ")
+    f.write(str(d[index]['image']))
+    f.write('\n')
+    f.write("x: ")
+    f.write(str(d[index]['x']))
+    f.write('\n')
+    f.write("y: ")
+    f.write(str(d[index]['y']))
+    f.write('\n')
+    f.write("z: ")
+    f.write(str(d[index]['z']))
+    f.write('\n')
+
 
 
 
