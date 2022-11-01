651c05332d73c59497a2609d5a55ebc6c773f348
SaraTaxy <sara5tassinari@gmail.com>
2022-11-01 09:14:41 +0100

    find the parallelepiped containing all image information finale

--- funzione.py
+++ funzione.py
@@ -24,7 +24,7 @@ def pre_processing_T2(data_path):
 
     final_hist = []    #list of value pixel  != 0   --> need to histogram
 
-    for images in os.listdir(data_path):
+    for images in os.listdir(data_path):                        #images in the folder
         dict_mask = {}
         pixel = {}
 
@@ -32,67 +32,69 @@ def pre_processing_T2(data_path):
         path_image = os.path.join(data_path, images)
         if path_image.find('DS_Store') != -1:
             continue
-
-        T2_image = nib.load(path_image).get_fdata()
-        T2_image = np.rot90(T2_image)
-        nd = np.ndim(T2_image)
-
-        max_pixel = np.amax(T2_image)
-        min_pixel = np.amin(T2_image)
-
-        mask = ((T2_image[:, :, :]) > 0)
-
-        i_list = []
-        j_list = []
-
-
-        for z in range(0, mask.shape[2]):
-            for i in range(0, mask.shape[0]):
-                for j in range(0, mask.shape[1]):
-                    if mask[i][j][z] == True:
-                        j_list.append(j)
-                        i_list.append(i)
-
-        max_x = np.amax(i_list)
-        max_y = np.amax(j_list)
-
-
-        min_x = np.amin(i_list)
-        min_y = np.amin(j_list)
-
-
-        T2_image_cropped = T2_image[min_x:max_x, min_x:max_x, 0:46]
-
-        T2_image_final = 0
-
-        T2_total.append(T2_image_cropped[T2_image_cropped != 0])
-
-        dict_mask = {"num image": p,
-                     "image": images,
-                     "x": [np.amin(i_list), np.amax(i_list)],
-                     "y": [np.amin(j_list), np.amax(j_list)],
-                     "delta_x": (np.amax(i_list) - np.amin(i_list)),
-                     "delta_y": (np.amax(j_list) - np.amin(j_list)),
-                     "dim image begin": np.shape(T2_image),
-                     "dim image cropped": np.shape(T2_image_cropped),
-                     "dim image +5 pixel x and y": T2_image_final,
-                     "imag": T2_image}
-
-        d.append(dict_mask)
-
-        pixel = {"image": images,
-                 "value pixel max": max_pixel,
-                 "value pixel min": min_pixel}
-        o.append(pixel)
-
         p = p + 1
-        print(p)
+        if p<=2:
+            T2_image = nib.load(path_image).get_fdata()    #load image
+            T2_image = np.rot90(T2_image)                  #rotation image
+            nd = np.ndim(T2_image)
 
-        '''
-        for elem in T2_total:
-            for l in elem:
-                final_hist.append(l)
-        '''
+            max_pixel = np.amax(T2_image)                 #find pixel max
+            min_pixel = np.amin(T2_image)                 #find pixel min
+
+            mask = ((T2_image[:, :, :]) > 0)              #create a mask
+
+            i_list = []
+            j_list = []
+
+
+            for z in range(0, mask.shape[2]):
+                for i in range(0, mask.shape[0]):
+                    for j in range(0, mask.shape[1]):
+                        if mask[i][j][z] == True:
+                            j_list.append(j)
+                            i_list.append(i)
+
+            max_x = np.amax(i_list)
+            max_y = np.amax(j_list)
+
+
+            min_x = np.amin(i_list)
+            min_y = np.amin(j_list)
+
+
+            T2_image_cropped = T2_image[min_x:max_x, min_x:max_x, 0:46]
+
+            T2_image_final = 0
+
+            T2_total.append(T2_image_cropped[T2_image_cropped != 0])
+
+            dict_mask = {"num image": p,
+                         "image": images,
+                         "x": [np.amin(i_list), np.amax(i_list)],
+                         "y": [np.amin(j_list), np.amax(j_list)],
+                         "delta_x": (np.amax(i_list) - np.amin(i_list)),
+                         "delta_y": (np.amax(j_list) - np.amin(j_list)),
+                         "dim image begin": np.shape(T2_image),
+                         "dim image cropped": np.shape(T2_image_cropped),
+                         "dim image +5 pixel x and y": T2_image_final,
+                         "imag": T2_image}
+
+
+            d.append(dict_mask)
+
+            pixel = {"image": images,
+                     "value pixel max": max_pixel,
+                     "value pixel min": min_pixel}
+            o.append(pixel)
+
+            #p = p + 1
+            print(p)
+
+            '''
+            for elem in T2_total:
+                for l in elem:
+                    final_hist.append(l)
+            '''
         max_x = 0
         index_x = 0
         max_y = 0
@@ -113,6 +115,7 @@ def pre_processing_T2(data_path):
                 index_y = i
             else:
                 max_y = max_y
+
     return d, im_x, max_x, im_y, max_y, final_hist, o
 
 
@@ -120,8 +123,10 @@ data = "data/raw/T2_images"
 d, im_x, max_x, im_y, max_y, final_hist, o = pre_processing_T2(data)
 
 
+
+
 #file info mask images
-outFileName = "./pre_processing_T2_images/mask_dimension_T2_images.txt"
+outFileName = "./pre_processing_T2_images/mask_dimension_T2_images_prova.txt"
 f = open(outFileName, "w")
 for elem in d:
     f.write(str(elem))
@@ -130,14 +135,14 @@ f.write('\n')
 # find max value of images volum
 f.write("----------------------------------------DIM MASK MAX VOLUM IMAGE-----------------------------------------------------")
 f.write('\n')
-f.write("index image x max: ")
-f.write(str(im_x))
+#f.write("index image x max: ")
+#f.write(str(im_x))
 f.write('\n')
 f.write("delta_x: ")
 f.write(str(max_x))
 f.write('\n')
-f.write("index image y max: ")
-f.write(str(im_y))
+#f.write("index image y max: ")
+#f.write(str(im_y))
 f.write('\n')
 f.write("delta_y: ")
 f.write(str(max_y))
@@ -195,7 +200,7 @@ for i in range(0, len(d)):
 f.close()
 
 #file info pixel images
-outFile = "./pre_processing_T2_images/Pixel.txt"
+outFile = "./pre_processing_T2_images_50_100/Pixel.txt"
 g = open(outFile, "w")
 for elem in o:
     g.write(str(elem))
@@ -209,6 +214,209 @@ plt.hist(final_hist)
 plt.savefig('./pre_processing_T2_images/instogram_t2_final')
 
 
+
+
+#----------------------------------- MAX AND MIN PIXEL IMAGES ----------------------------------------------------------
+def max_min_pixel(data_path):
+    max_im = 0
+    min_im = 0
+    #index_max_pixel = ''
+    d = []  # list info images
+    p = 0
+    max_pixel = 0
+    min_pixel = 0
+
+    for images in os.listdir(data_path):
+        dict_mask = {}
+
+
+        path_image = os.path.join(data_path, images)
+        if path_image.find('DS_Store') != -1:
+            continue
+        p = p + 1
+        print(p)
+        T2_image = nib.load(path_image).get_fdata()
+        max_pixel = np.amax(T2_image)
+        min_pixel = np.amin(T2_image)
+
+        dict_mask = {"image": images,
+                     "max_pixel": max_pixel,
+                     "min pixel": min_pixel}
+
+        d.append(dict_mask)
+
+        if dict_mask["max_pixel"] > max_im:
+            max_im = dict_mask["max_pixel"]
+            index_max_pixel = dict_mask["image"]
+        else:
+            max_pixel = max_pixel
+
+    outFile = "./pre_processing_T2_images/Pixel.txt"
+    g = open(outFile, "w")
+    for elem in d:
+        g.write(str(elem))
+        g.write('\n')
+    g.write('\n')
+    g.write("---------")
+    g.write('\n')
+    g.write("max pixel: ")
+    g.write('\n')
+    g.write(str(max_im))
+    g.write('\n')
+    g.write("max pixel index image: ")
+    g.write('\n')
+    g.write(str(index_max_pixel))
+    g.close()
+
+
+data = "data/raw/T2_images"
+max_min_pixel(data)
+
+
+#-----------------------------------------------------new correct ------------------------------------------------------
+
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+data_path = "data/raw/T2_images"
+
+d = []  #list info images
+p = 0   #counter images
+
+max_delta_x = 0
+max_delta_y = 0
+
+for images in os.listdir(data_path):  # images in the folder
+    dict_mask = {}
+
+    path_image = os.path.join(data_path, images)
+    if path_image.find('DS_Store') != -1:
+        continue
+    p = p + 1
+    if p <= 5:
+        print(p)
+        T2_image = nib.load(path_image).get_fdata()  # load image
+
+        mask = (T2_image[:, :, :] != 0)
+        y = np.where(np.any(mask, axis=0))[0]
+        y_min, y_max = y[[0, -1]]
+
+        x = np.where(np.any(mask, axis=1))[0]
+        x_min, x_max = x[[0, -1]]
+
+
+        delta_x = x_max - x_min
+        delta_y = y_max - y_min
+
+
+        dict_mask = {"num image": p,
+                     "image": images,
+                     "x": [x_min, x_max],
+                     "y": [y_min, y_max],
+                     "delta_x": delta_x,
+                     "delta_y": delta_y,
+                     "pixel_max":  np.amax(T2_image),
+                     "pixel_min": np.amin(T2_image),
+                     "im": T2_image}
+
+        d.append(dict_mask)
+
+
+outFileName = "./pre_processing_T2_images/mask_dimension_T2_images_prova.txt"
+f = open(outFileName, "w")
+for elem in d:
+
+    f.write(str(elem))
+    f.write('\n')
+    if elem['delta_x']> max_delta_x:
+        max_delta_x = elem["delta_x"]
+        id_max_delta_x = elem["image"]
+    if elem['delta_y']> max_delta_y:
+        max_delta_y = elem["delta_y"]
+        id_max_delta_y = elem["image"]
+f.write('\n')
+f.write("----------------------------------------DIM MASK MAX VOLUM IMAGE-----------------------------------------------------")
+f.write('\n')
+f.write("index image x max: ")
+f.write(str(id_max_delta_x))
+f.write('\n')
+f.write("delta_x: ")
+f.write(str(max_delta_x))
+f.write('\n')
+f.write("index image y max: ")
+f.write(str(id_max_delta_y))
+f.write('\n')
+f.write("delta_y: ")
+f.write(str(max_delta_y))
+f.write('\n')
+# crop image in the center
+x_star = int(max_delta_x / 2)
+y_star = int(max_delta_y / 2)
+
+f.write("----------------------------------------END MASK FOR EACH IMAGES-----------------------------------------------------")
+
+
+
+for i in d:
+    x_min_im = int(i['x'][0]) + int(i['delta_x'] / 2) - x_star
+    x_max_im = int(i['x'][0]) + int(i['delta_x'] / 2) + x_star
+
+    y_min_im = int(i['y'][0]) + int(i['delta_y'] / 2) - y_star
+    y_max_im = int(i['y'][0]) + int(i['delta_y'] / 2) + y_star
+
+
+    T2_image_final = i['im'][x_min_im:x_max_im, y_min_im:y_max_im, :]
+
+    dim1 = np.zeros((5, np.shape(T2_image_final)[1], np.shape(T2_image_final)[2]))
+
+    T2_image_final = np.vstack((dim1, T2_image_final))
+    T2_image_final = np.vstack((T2_image_final, dim1))
+
+    dim2 = np.zeros((np.shape(T2_image_final)[0], 5, np.shape(T2_image_final)[2]))
+    T2_image_final = np.hstack((dim2, T2_image_final))
+    T2_image_final = np.hstack((T2_image_final, dim2))
+
+
+    def pad_with(vector, pad_width, iaxis, kwargs):
+        pad_value = kwargs.get('padder', 0)
+        vector[:pad_width[0]] = pad_value
+        vector[-pad_width[1]:] = pad_value
+
+
+    a = np.arange(6)
+    a = a.reshape((2, 3))
+    np.pad(a, 2, pad_with)
+
+
+    f.write('\n')
+    f.write("x_min start: ")
+    f.write(str(x_min_im))
+    f.write("   x_max start: ")
+    f.write(str(x_max_im))
+    f.write('\n')
+    f.write("y_min start: ")
+    f.write(str(y_min_im))
+    f.write("   y_max start: ")
+    f.write(str(y_max_im))
+    f.write('\n')
+    f.write("final image: ")
+    f.write(str(np.shape(T2_image_final)))
+    f.write('\n')
+    f.write("x_min_new: ")
+    f.write(str(x_min_im - 5))
+    f.write("   x_max_new: ")
+    f.write(str(x_max_im + 5))
+    f.write('\n')
+    f.write("y_min_new: ")
+    f.write(str(y_min_im - 5))
+    f.write("   y_max_new: ")
+    f.write(str(y_max_im + 5))
+    f.write('\n')
+    f.write("----------------------------------------------------------------------------------------------------------------------")
+f.close()
 
 
 
--- script_volum_images_T2.py
+++ script_volum_images_T2.py
@@ -1,35 +1,42 @@
-def crop_center(data, out_sp):
-    """
-    crop_center --> dal centro verso tutte le altre dimensioni
-    Returns the center part of volume data.
-    crop: in_sp > out_sp
-    Example:
-    data.shape = np.random.rand(182, 218, 182)
-    out_sp = (160, 192, 160)                      #dimensione output finale
-    data_out = crop_center(data, out_sp)
-    """
-    in_sp = data.shape
-    nd = np.ndim(data)  #x,y,z =3 almeno dovrebbe essere
-    x_crop = int((in_sp[-3] - out_sp[-3]) / 2)
-    y_crop = int((in_sp[-2] - out_sp[-2]) / 2)
-    #z_crop = int((in_sp[-3] - out_sp[-1]) / 2)
-    if nd == 3:
-        data_crop = data[x_crop:-x_crop, y_crop:-y_crop, out_sp[-1]:-out_sp[-1]]
-    elif nd == 4:
-        data_crop = data[:, x_crop:-x_crop, y_crop:-y_crop, out_sp[-1]:-out_sp[-1]]
-    else:
-        raise ('Wrong dimension! dim=%d.' % nd)
-    return data_crop
+
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+
+T2_image = nib.load('data/raw/T2_images/RecordID_0015_T2_flair.nii').get_fdata()
+mask = (T2_image[:,:,:] != 0)
+y = np.where(np.any(mask, axis=0))[0]
+y_min, y_max = y[[0, -1]]
+
+x = np.where(np.any(mask, axis=1))[0]
+x_min, x_max = x[[0, -1]]
 
 
-def format_image_T2(data):
-    image = np.rot90(data)
-    nd = np.ndim(data)    #dimension
-    shape = data.shape
-    val_min = np.amin(data)
-    val_max = np.amax(data)
+plt.imshow(T2_image[x_min:x_max, y_min:y_max, 12])
+plt.show()
 
-    return image, nd, shape, val_min, val_max
+max_delta_x = 0
+max_delta_y = 0
+
+delta_x = x_max -x_min
+delta_y = y_max -y_min
+
+
+if delta_x > max_delta_x:
+    max_delta_x = delta_x
+    #salva l'indice dell'immagine
+
+if delta_y > max_delta_y:
+    max_delta_y = delta_y
+    #salva l'indice dell'immagine
+
+print(max_delta_x)
+print(max_delta_y)
+
+
+
+
+
 
 
 
@@ -37,11 +44,45 @@ import nibabel as nib
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 T2_image = nib.load('data/raw/T2_images/RecordID_0015_T2_flair.nii').get_fdata()
 T2_image = np.rot90(T2_image)
 nd = np.ndim(T2_image)
 print(nd)
 print(T2_image.shape)
+
+a= []
+l = []
+k =[]
+
+
+T2_image_1 = nib.load('data/raw/T2_images/RecordID_0012_T2_flair.nii').get_fdata()
+T2_image_1 = np.rot90(T2_image_1)
+T2 = np.hstack((T2_image, T2_image_1))
+
+T2_image_2 = nib.load('data/raw/T2_images/RecordID_0016_T2_flair.nii').get_fdata()
+T2 = np.hstack((T2, T2_image_2))
+T2_image_3 = nib.load('data/raw/T2_images/RecordID_0021_T2_flair.nii').get_fdata()
+T2 = np.hstack((T2, T2_image_3))
+T2_image_4 = nib.load('data/raw/T2_images/RecordID_0022_T2_flair.nii').get_fdata()
+T2 = np.hstack((T2, T2_image_4))
+plt.hist(T2_image.flatten())
+#plt.savefig('./report/istogrammi_t2')
+plt.show()
+
+with open("esempio.txt", 'w') as f:
+    for elem in T2_image:
+        if elem != 0:
+            l.append(elem)
+        k.append(l)
+
+    f.write(str(k))
+    plt.hist(k.flatten())
+    # plt.savefig('./report/istogrammi_t2')
+    plt.show()
+
+
+
 
 
 
@@ -67,19 +108,29 @@ plt.show()
 #find value pixel max/min
 
 max_pixel = np.amax(T2_image)
-print(max_pixel)
+
+max_1= T2_image.max()
+print(max_1)
 
 min_pixel = np.amin(T2_image)
 print(min_pixel)
 
-#crop  --> sbagliato perè do le dimensioni in ingresso
 
-crop_T2 = crop_center(T2_image,(325,250,1))
-plt.imshow(crop_T2[:,:, 33])
+
+
+#histrogram
+T2_2 = nib.load('data/raw/T2_images/RecordID_0012_T2_flair.nii').get_fdata()
+
+#T2 = np.vstack(T2_image, T2_2)
+plt.hist(T2_2.flatten())
+#plt.savefig('./report/istogrammi_t2')
+plt.show()
+
+
 
 #create a mask
-
-mask = (T2_image[:,:,layer])>0
+layer=33
+mask = (T2_image[:,:,layer]) != 0
 plt.subplot(1,2,1)
 plt.imshow(T2_image[:,:,layer])
 plt.title("Immagine iniziale")
@@ -148,18 +199,30 @@ dim_mask = {"immagine": 1, "nome immagine": "data/raw/T2_images/RecordID_0012_T2
 
 
 T2_image_cropped=T2_image[p_min_xy[0]:p_max_xy[0],p_min_yx[0]:p_max_yx[0], layer]
+print(np.shape(T2_image_cropped))
+dim1 = np.zeros((5,np.shape(T2_image_cropped)[1]))
+dim2 = np.zeros((np.shape(T2_image_cropped)[0],5))
+
+T2_image_crop=np.vstack((dim1, T2_image_cropped))
+T2_image_crop=np.vstack((T2_image_crop, dim1))
+T2_image_crop=np.hstack((dim2, T2_image_crop))
+T2_image_crop=np.hstack((T2_image_crop, dim2))
+print(np.shape(T2_image_crop))
 mask=np.transpose(mask)
 
-plt.subplot(1,3,1)
+plt.subplot(1,4,1)
 plt.imshow(T2_image[:,:,layer])
 plt.title("Immagine iniziale")
-plt.subplot(1,3,2)
+plt.subplot(1,4,2)
 plt.imshow(mask)
 plt.title("mask")
-plt.subplot(1,3,3)
+plt.subplot(1,4,3)
 plt.imshow(T2_image_cropped)
 plt.title("immagine tagliata")
-
+plt.subplot(1,4,4)
+plt.imshow(T2_image_crop)
+plt.title("immagine +5")
+plt.style.use('grayscale')
 
 mask_3D = (T2_image[:,:,:])>0
 print(mask_3D.shape)
@@ -285,15 +348,22 @@ data_path = "data/raw/T2_images"
 
 d=[]
 p=0
-
+max_x = 0
+index_x = 0
+max_y = 0
+index_y = 0
+max_z = 0
+index_z = 0
 #dict_mask = {}
+o = []
+
+T2_total=[]
 for images in os.listdir(data_path):
     dict_mask = {}
+    pixel ={}
 
 
-    if p<=10:
-        p = p + 1
-        print(p)
+    if p<=3:
         path_image = os.path.join(data_path, images)
         if path_image.find('DS_Store') != -1:
             continue
@@ -301,6 +371,7 @@ for images in os.listdir(data_path):
         T2_image = nib.load(path_image).get_fdata()
         T2_image = np.rot90(T2_image)
         nd = np.ndim(T2_image)
+
         max_pixel = np.amax(T2_image)
         min_pixel = np.amin(T2_image)
 
@@ -325,7 +396,20 @@ for images in os.listdir(data_path):
         print("y min: ", np.amin(j_list), "y max: ", np.amax(j_list))
         print("z min: ", np.amin(z_list), "z max: ", np.amax(z_list))
         '''
-        vol = (np.amax(i_list)-np.amin(i_list))*(np.amax(j_list)-np.amin(j_list))*(np.amax(z_list)-np.amin(z_list))
+
+        max_x = np.amax(i_list)
+        max_y = np.amax(j_list)
+        max_z = np.amax(z_list)
+
+        min_x = np.amin(i_list)
+        min_y = np.amin(j_list)
+        min_z = np.amin(z_list)
+
+        T2_image_cropped = T2_image[min_x:max_x, min_x:max_x, 0:46]
+
+        T2_image_final = 0
+
+        T2_total.append(T2_image_cropped[T2_image_cropped!=0])
 
         #print("vol in pixel: ", vol)
         dict_mask = {"num image": p,
@@ -333,16 +417,29 @@ for images in os.listdir(data_path):
                      "x": [np.amin(i_list), np.amax(i_list)],
                      "y": [np.amin(j_list), np.amax(j_list)],
                      "z": [np.amin(z_list), np.amax(z_list)],
-                     "Pixel_Vol": vol,
                      "delta_x": (np.amax(i_list) - np.amin(i_list)),
                      "delta_y": (np.amax(j_list) - np.amin(j_list)),
-                     "delta_z": (np.amax(z_list) - np.amin(z_list))}
+                     "dim image begin" : np.shape(T2_image),
+                     "dim image cropped": np.shape(T2_image_cropped),
+                     "dim image +5 pixel x and y": T2_image_final,
+                     "imag": T2_image}
+
         d.append(dict_mask)
-    else:
+
+        pixel = {"image": images,
+                 "value pixel max": max_pixel,
+                 "value pixel min": min_pixel}
+        o.append(pixel)
+
         p = p + 1
+        print(p)
 
-print(d)
-
+j=[]
+for elem in T2_total:
+    for i in elem:
+        j.append(i)
+plt.hist(j)
+plt.show()
 
 max_x = 0
 index_x = 0
@@ -365,37 +462,122 @@ for i in range(0, len(d)):
     else:
         max_y = max_y
 
-    if d[i]['delta_z'] > max_z:
-        max_z = d[i]['z'][1] - d[i]['z'][0]
-        im_z=d[i]['image']
-        index_z = i
-    else:
-        max_z = max_z
 
-
-
-#crop image in the center
-x_star = int(max_x/2)
-y_star = int(max_y/2)
-z_star = int(max_z/2)
-
-
-for i in range(0, len(d)):
-
-    x_min_im = int(d[i]['x'][0]) + int(d[i]['delta_x']/2) - x_star
-    x_max_im = int(d[i]['x'][0]) + int(d[i]['delta_x']/2) + x_star
-    #print("delta_x im ", i)
-    #print([x_min_im, x_max_im])
-
-    y_min_im = int(d[i]['y'][0]) + int(d[i]['delta_y'] / 2) - y_star
-    y_max_im = int(d[i]['y'][0]) + int(d[i]['delta_y'] / 2) + y_star
-
-    z_min_im = int(d[i]['z'][0]) + int(d[i]['delta_z'] / 2) - z_star
-    z_max_im = int(d[i]['z'][0]) +int(d[i]['delta_z'] / 2) + z_star
 
 
 
 # create file
+outFileName="./pre_processing_T2_images/mask_dimension_T2_images.txt"
+f=open(outFileName, "w")
+for elem in d:
+    f.write(str(elem))
+    f.write('\n')
+f.write('\n')
+# find max value of images volum
+f.write("----------------------------------------DIM MASK MAX VOLUM IMAGE-----------------------------------------------------")
+f.write('\n')
+f.write("index image x max: ")
+f.write(str(im_x))
+f.write('\n')
+f.write("delta_x: ")
+f.write(str(max_x))
+f.write('\n')
+f.write("index image y max: ")
+f.write(str(im_y))
+f.write('\n')
+f.write("delta_y: ")
+f.write(str(max_y))
+f.write('\n')
+# crop image in the center
+x_star = int(max_x / 2)
+y_star = int(max_y / 2)
+
+f.write("----------------------------------------END MASK FOR EACH IMAGES-----------------------------------------------------")
+
+for i in range(0, len(d)):
+    x_min_im = int(d[i]['x'][0]) + int(d[i]['delta_x'] / 2) - x_star
+    x_max_im = int(d[i]['x'][0]) + int(d[i]['delta_x'] / 2) + x_star
+    # print("delta_x im ", i)
+    # print([x_min_im, x_max_im])
+
+    y_min_im = int(d[i]['y'][0]) + int(d[i]['delta_y'] / 2) - y_star
+    y_max_im = int(d[i]['y'][0]) + int(d[i]['delta_y'] / 2) + y_star
+
+    T2_image_final = d[i]['imag'][x_min_im:x_max_im, y_min_im:y_max_im, :]
+
+    dim1 = np.zeros((5, np.shape(T2_image_final)[1], np.shape(T2_image_final)[2]))
+
+    T2_image_final = np.vstack((dim1, T2_image_final))
+    T2_image_final = np.vstack((T2_image_final, dim1))
+    dim2 = np.zeros((np.shape(T2_image_final)[0], 5, np.shape(T2_image_final)[2]))
+    T2_image_final = np.hstack((dim2, T2_image_final))
+    T2_image_final = np.hstack((T2_image_final, dim2))
+
+
+
+    f.write('\n')
+    f.write("x_min: ")
+    f.write(str(x_min_im))
+    f.write(" x_max: ")
+    f.write(str(x_max_im))
+    f.write('\n')
+    f.write("y_min: ")
+    f.write(str(y_min_im))
+    f.write(" y_max: ")
+    f.write(str(y_max_im))
+    f.write('\n')
+    f.write("final image: ")
+    f.write(str(np.shape(T2_image_final)))
+    f.write('\n')
+    f.write("x_min_new: ")
+    f.write(str(x_min_im-5))
+    f.write(" x_max_new: ")
+    f.write(str(x_max_im+5))
+    f.write('\n')
+    f.write("y_min_new: ")
+    f.write(str(y_min_im-5))
+    f.write(" y_max_new: ")
+    f.write(str(y_max_im+5))
+    f.write('\n')
+    f.write("----------------------------------------------------------------------------------------------------------------------")
+f.close()
+
+
+
+#save pixel immagine
+outFile="./pre_processing_T2_images/Pixel.txt"
+g=open(outFile, "w")
+for elem in o:
+    g.write(str(elem))
+    g.write('\n')
+g.write('\n')
+g.close()
+
+#save histogram
+plt.hist(j)
+#plt.show()
+plt.savefig('./pre_processing_T2_images/instogram_t2_final')
+
+
+with open("Hystogram_T2_images.txt", 'w') as f:
+    for elem in o:
+        f.write(str(elem))
+        f.write('\n')
+    f.write('\n')
+    plt.hist(j)
+    plt.show()
+    plt.savefig('./pre_processing_T2_images/instogram_t2_final')
+
+
+
+
+
+
+
+
+
+
+
 with open("mask_dimension_T2_images.txt", 'w') as f:
     for elem in d:
         f.write(str(elem))
@@ -404,9 +586,6 @@ with open("mask_dimension_T2_images.txt", 'w') as f:
 
     # find max value of images volum
     f.write("----------------------------------------DIM MASK MAX VOLUM IMAGE-----------------------------------------------------")
-    f.write('\n')
-    f.write("volum max: ")
-    f.write(str(max))
     f.write('\n')
     f.write("index image x max: ")
     f.write(str(im_x))
@@ -420,20 +599,14 @@ with open("mask_dimension_T2_images.txt", 'w') as f:
     f.write("delta_y: ")
     f.write(str(max_y))
     f.write('\n')
-    f.write("index image z max: ")
-    f.write(str(im_z))
-    f.write('\n')
-    f.write("delta_z: ")
-    f.write(str(max_z))
-    f.write('\n')
+
+
 
     # crop image in the center
-    x_star = int(max_x/2)
-    y_star = int(max_y/2)
-    z_star = int(max_z/2)
+    x_star = int(max_x / 2)
+    y_star = int(max_y / 2)
 
     f.write("----------------------------------------END MASK FOR EACH IMAGES-----------------------------------------------------")
-    f.write('\n')
 
     for i in range(0, len(d)):
         x_min_im = int(d[i]['x'][0]) + int(d[i]['delta_x'] / 2) - x_star
@@ -441,13 +614,22 @@ with open("mask_dimension_T2_images.txt", 'w') as f:
         # print("delta_x im ", i)
         # print([x_min_im, x_max_im])
 
-        y_min_im = int(d[i]['y'][0]) +int(d[i]['delta_y'] / 2) - y_star
+        y_min_im = int(d[i]['y'][0]) + int(d[i]['delta_y'] / 2) - y_star
         y_max_im = int(d[i]['y'][0]) + int(d[i]['delta_y'] / 2) + y_star
 
-        z_min_im = int(d[i]['z'][0]) + int(d[i]['delta_z'] / 2) - z_star
-        z_max_im = int(d[i]['z'][0]) + int(d[i]['delta_z'] / 2) + z_star
+        T2_image_final = d[i]['imag'][x_min_im:x_max_im, y_min_im:y_max_im, :]
+
+        dim1 = np.zeros((5, np.shape(T2_image_final)[1], np.shape(T2_image_final)[2]))
+
+        T2_image_final = np.vstack((dim1, T2_image_final))
+        T2_image_final = np.vstack((T2_image_final, dim1))
+        dim2 = np.zeros((np.shape(T2_image_final)[0], 5, np.shape(T2_image_final)[2]))
+        T2_image_final = np.hstack((dim2, T2_image_final))
+        T2_image_final = np.hstack((T2_image_final, dim2))
 
 
+
+        f.write('\n')
         f.write("x_min: ")
         f.write(str(x_min_im))
         f.write(" x_max: ")
@@ -458,13 +640,51 @@ with open("mask_dimension_T2_images.txt", 'w') as f:
         f.write(" y_max: ")
         f.write(str(y_max_im))
         f.write('\n')
-        f.write("z_min: ")
-        f.write(str(z_min_im))
-        f.write(" z_max: ")
-        f.write(str(z_max_im))
+        f.write("final image: ")
+        f.write(str(np.shape(T2_image_final)))
         f.write('\n')
-        f.write("------------------------------")
+        f.write("x_min_new: ")
+        f.write(str(x_min_im-5))
+        f.write(" x_max_new: ")
+        f.write(str(x_max_im+5))
         f.write('\n')
+        f.write("y_min_new: ")
+        f.write(str(y_min_im-5))
+        f.write(" y_max_new: ")
+        f.write(str(y_max_im+5))
+        f.write('\n')
+        f.write("----------------------------------------------------------------------------------------------------------------------")
+
+
+
+with open("Hystogram_T2_images.txt", 'w') as f:
+    for elem in o:
+        f.write(str(elem))
+        f.write('\n')
+    f.write('\n')
+    plt.hist(j)
+    plt.show()
+    plt.savefig('./reports/pre_processing_T2/instogram_t2_final')
+
+
+
+
+outFileName="reports/instogram_t2/nome.txt"
+outFile=open(outFileName, "w")
+outFile.write("""Hello my name is ABCD""")
+outFile.close()
+
+
+#-----------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
 
 
 with open("mask_dimension_T2_images.txt", 'w') as f:
@@ -486,9 +706,6 @@ with open("mask_dimension_T2_images.txt", 'w') as f:
             max=max
 
     f.write('\n')
-    f.write("volum max: ")
-    f.write(str(max))
-    f.write('\n')
     f.write("index image vol max: ")
     f.write(str(d[index]['image']))
     f.write('\n')
@@ -501,6 +718,8 @@ with open("mask_dimension_T2_images.txt", 'w') as f:
     f.write("z: ")
     f.write(str(d[index]['z']))
     f.write('\n')
+
+
 
 
 
@@ -554,3 +773,21 @@ for q in range(0, len(l)):
         max=max
 
 print(nome[i], max)
+
+
+
+
+
+#zero padding
+
+
+def pad_with(vector, pad_width, iaxis, kwargs):
+    pad_value = kwargs.get('padder', 0)
+    vector[:pad_width[0]] = pad_value
+    vector[-pad_width[1]:] = pad_value
+a = np.arange(6)
+a = a.reshape((2, 3))
+np.pad(a, 2, pad_with)
+
+
+
